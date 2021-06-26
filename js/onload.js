f8ca4edbfc6ebb38e422c9b8c75f5fa973f66784
Soomin Shin <ssoomin1@naver.com>
2021-06-26 23:47:30 +0900

    교무실-뒤로가기 버튼 구현

---
+++ js/onload.js
@@ -0,0 +1,14 @@
+function onloadChoose(){
+    let go_back=confirm("정말로 교무실로 돌아가겠습니까?");
+    if(go_back===true){
+        location.href="../html/chooseGame.html";
+    }
+}
+
+function onloadclass(){
+    location.href="../html/class1.html";
+}
+
+function onloadmain(){
+    location.href="../html/main.html";
+}
